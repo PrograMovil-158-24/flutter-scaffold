c5ce1b0aa0872deb9e19d5af4ad345df77fb55ed
Ivan Cruz <ivancruzhn@gmail.com>
2024-01-19 20:41:38 -0600

    proyecto base en flutter

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(const MyApp());
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}):super(key: key);
+  @override
+  build(context){
+    return const Text('Hola');
+  }
+}
